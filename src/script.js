08bd8fb2a63b9adeccf712683ae131e606655db0
axelquartz <axelarep@gmail.com>
2024-12-11 11:42:18 -0600

    Display error in DOM

--- src/script.js
+++ src/script.js
@@ -1,31 +1,46 @@
 const catImage = document.getElementById("cat-image-1");
 const catImage2 = document.getElementById("cat-image-2");
-const catImage3 = document.getElementById("cat-image-3");
 const catButton = document.getElementById("generate-cats-button");
-const URL = "https://api.thecatapi.com/v1/images/search?limit=3&breed_ids=siam&api_key=live_KLPbsvkAcNRHqVoNHvclFCYyDUu0KxlF1AaDr3mXckeJCWQivtx2uO7wdUX259E4";
+const errorDisplay = document.getElementById("error-display");
+const API_KEY = "api_key=live_KLPbsvkAcNRHqVoNHvclFCYyDUu0KxlF1AaDr3mXckeJCWQivtx2uO7wdUX259E4";
+const URL_RANDOM = `https://api.thecatapi.com/v1/images/search?limit=2&breed_ids=siam&${API_KEY}`;
+const URL_FAVORITES = `https://api.thecatapi.com/v1/favourites?&`;
 
-async function fetchCats() {
+async function fetchRandomCats() {
   try {
-    const response = await fetch(URL);
+    const response = await fetch(URL_RANDOM, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     const data = await response.json();
     console.log(data);
     catImage.src = data[0].url;
     catImage2.src = data[1].url;
-    catImage3.src = data[2].url;
   } catch (error) {
     console.log(error);
+    errorDisplay.textContent = error.message;
   }
 }
 
-catButton.addEventListener("click", fetchCats);
+async function fetchFavorites() {
+  try {
+    const response = await fetch(URL_FAVORITES, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+    errorDisplay.textContent = error.message;
+  }
+}
 
-// catButton.addEventListener("click", fetchCats);
+catButton.addEventListener("click", fetchRandomCats);
 
-// function fetchCats() {
-//   fetch(URL)
-//     .then((response) => response.json())
-//     .then((data) => (catImage.src = data[0].url))
-//     .catch((err) => console.log(err));
-// }
-
-fetchCats();
+fetchRandomCats();
+fetchFavorites();
