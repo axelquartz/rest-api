53e37829843a9c4e5cbf3ec2b59dd339936b41d4
axelquartz <axelarep@gmail.com>
2024-12-11 11:54:37 -0600

    Refacture async await functions removing try and catch blocks

--- src/script.js
+++ src/script.js
@@ -4,40 +4,39 @@ const catButton = document.getElementById("generate-cats-button");
 const errorDisplay = document.getElementById("error-display");
 const API_KEY = "api_key=live_KLPbsvkAcNRHqVoNHvclFCYyDUu0KxlF1AaDr3mXckeJCWQivtx2uO7wdUX259E4";
 const URL_RANDOM = `https://api.thecatapi.com/v1/images/search?limit=2&breed_ids=siam&${API_KEY}`;
-const URL_FAVORITES = `https://api.thecatapi.com/v1/favourites?&`;
+const URL_FAVORITES = `https://api.thecatapi.com/v1/favourites?&${API_KEY}`;
 
 async function fetchRandomCats() {
-  try {
-    const response = await fetch(URL_RANDOM, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-    catImage.src = data[0].url;
-    catImage2.src = data[1].url;
-  } catch (error) {
-    console.log(error);
-    errorDisplay.textContent = error.message;
+  const response = await fetch(URL_RANDOM, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.status !== 200) {
+    errorDisplay.textContent = `Error: ${response.status}`;
+    return;
   }
+  const data = await response.json();
+  console.log(data);
+  catImage.src = data[0].url;
+  catImage2.src = data[1].url;
+  console.log(`Status: ${response.status}`);
 }
 
 async function fetchFavorites() {
-  try {
-    const response = await fetch(URL_FAVORITES, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await response.json();
-    console.log(data);
-  } catch (error) {
-    console.log(error);
-    errorDisplay.textContent = error.message;
+  const response = await fetch(URL_FAVORITES, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (response.status !== 200) {
+    errorDisplay.textContent = `Error: ${response.status}`;
+    return;
   }
+  const data = await response.json();
+  console.log(`Status: ${response.status}`);
 }
 
 catButton.addEventListener("click", fetchRandomCats);
